0031d3950a0b19d3de7c2758a250e410d080b957
bongjoo.seo <bongjoo.seo@samsung.com>
2023-02-25 14:31:29 +0900

    버전 최신화

--- src/pages/index.js
+++ src/pages/index.js
@@ -52,7 +52,7 @@ export const pageQuery = graphql`
         title
       }
     }
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+    allMarkdownRemark(sort: { frontmatter: { date: DESC } }) {
       edges {
         node {
           excerpt
